d4119bf326c4c0a76cfa2689419ab8c8c79a2195
thiyagukb <thiyagux.kesavan.balakrishnan@intel.com>
2021-09-01 17:07:20 +0530

    Build: Adding build script file for UniversalPayload build using Ovmf.
    Signed-off-by: Zhiguang Liu <zhiguang.liu@intel.com>

---
+++ build_qemu_pol.bat
@@ -0,0 +1,17 @@
+@setlocal
+
+@if "%1" == "entry" (
+  goto entry
+)
+@if "%1" == "ovmf" (
+  goto ovmf
+)
+
+
+:entry
+call py UefiPayloadPkg\UniversalPayloadBuild.py -t CLANGPDB 
+if not %ERRORLEVEL% == 0 exit /b 1
+
+
+:ovmf
+call build -p OvmfPkg\QemuUniversalPayload\OvmfPkgPol.dsc -a IA32 -a X64 -D DEBUG_ON_SERIAL_PORT -D UNIVERSAL_PAYLOAD=TRUE -t CLANGPDB -y ovmflog.txt
